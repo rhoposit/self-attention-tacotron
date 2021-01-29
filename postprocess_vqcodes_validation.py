f6ab9a2f1c311410abeba4f7b51007c954a16265
v-j-williams <v-j-williams@ygpu23.smg.nii.ac.jp>
2021-01-29 21:43:52 +0900

    trying to synthesize from validation

--- postprocess_vqcodes_validation.py
+++ postprocess_vqcodes_validation.py
@@ -7,14 +7,15 @@ np.set_printoptions(threshold=sys.maxsize)
 import os
 import re
 
+
+class PredictionResultCodes(namedtuple("PredictionResult",
+    ["id", "key", "codes", "codes_length", "codes_width", "ground_truth_codes","ground_truth_codes_length", "text"])): pass
+
 class PredictionResult(namedtuple("PredictionResult",
-                                  ["id", "key", "codes", "codes_length", "codes_width", "ground_truth_codes","ground_truth_codes_length", "text", "source", "source_length"])): pass
+    ["id", "key", "mel", "mel_length", "mel_width", "predicted_mel", "predicted_mel_length", "text"])):
+    pass
 
-
-def glob_re(pattern, strings):
-    return filter(re.compile(pattern).match, strings)
-
-def parse_prediction_result(proto):
+def parse_prediction_result_codes(proto):
     features = {
         'id': tf.FixedLenFeature((), tf.int64),
         'key': tf.FixedLenFeature((), tf.string),
@@ -30,14 +31,32 @@ def parse_prediction_result(proto):
     parsed_features = tf.parse_single_example(proto, features)
     return parsed_features
 
-def decode_prediction_result(parsed):
+
+def parse_prediction_result(proto):
+    features = {
+        'id': tf.FixedLenFeature((), tf.int64),
+        'key': tf.FixedLenFeature((), tf.string),
+        'mel': tf.FixedLenFeature((), tf.string),
+        'mel_length': tf.FixedLenFeature((), tf.int64),
+        'mel_width': tf.FixedLenFeature((), tf.int64),
+        'predicted_mel': tf.FixedLenFeature((), tf.string),
+        'predicted_mel_length': tf.FixedLenFeature((), tf.int64),
+        'text': tf.FixedLenFeature((), tf.string),
+    }
+    parsed_features = tf.parse_single_example(proto, features)
+    return parsed_features
+
+
+
+
+def decode_prediction_result_codes(parsed):
     source = tf.decode_raw(parsed['source'], tf.int64)
     codes = tf.decode_raw(parsed['codes'], tf.int32)
     codes_length = parsed["codes_length"]
     codes_width = parsed["codes_width"]
     ground_truth_codes = tf.decode_raw(parsed['ground_truth_codes'], tf.int32)
     ground_truth_codes_length = parsed["ground_truth_codes_length"]
-    return PredictionResult(
+    return PredictionResultCodes(
         id=parsed['id'],
         key=parsed['key'],
         codes=tf.reshape(codes, shape=tf.stack([codes_length, codes_width], axis=0)),
@@ -49,29 +68,47 @@ def decode_prediction_result(parsed):
         source=source,
         source_length=parsed['source_length'],
     )
-
+def decode_prediction_result(parsed):
+    codes_width = parsed['mel_width']
+    codes_length = parsed['predicted_mel_length']
+    codes = tf.decode_raw(parsed['predicted_mel'], tf.float32)
+    ground_truth_codes_length = parsed['mel_length']
+    ground_truth_codes = tf.decode_raw(parsed['mel'], tf.float32)
+    return PredictionResultCodes(
+        id=parsed['id'],
+        key=parsed['key'],
+        codes=tf.reshape(codes, shape=tf.stack([codes_length, codes_width], axis=0)),
+        codes_length=codes_length,
+        codes_width=codes_width,
+        ground_truth_codes=tf.reshape(ground_truth_codes, shape=tf.stack([ground_truth_codes_length, codes_width], axis=0)),
+        ground_truth_codes_length=ground_truth_codes_length,
+        text=parsed['text'],
+    )
 
 
 exp = sys.argv[1]
 steps = sys.argv[2]
-datadir = r'/gs/hs0/tgh-20IAA/jenn/taco_exp/checkpoints/'+exp+'/.*0'+steps+'_.*\.tfrecord'
+datadir = "/home/smg/v-j-williams/workspace/external_modified/checkpoints/"+exp+"_small/*0"+steps+"_*.tfrecord"
 print(datadir)
 tfiles = glob.glob(datadir)
-#tfiles = glob_re(r'.*0'+steps+'_.*\.tfrecord', os.listdir())
-
 print(tfiles)
-outdir = "/gs/hs0/tgh-20IAA/jenn/taco_exp/"+exp+"/"
+outdir = "/home/smg/v-j-williams/workspace/external_modified/synth/"+exp+"/"
 
 
 txtlist = []
 predlist = []
 truthlist = []
-synthoutdir = "/gs/hs0/tgh-20IAA/jenn/special/mt_lists/"
+synthoutdir = "/home/smg/v-j-williams/workspace/tsubame_work/special/mt_lists/"
 
 sess = tf.InteractiveSession()
 with sess.as_default():
     for record in tfiles:
         for example in tf.python_io.tf_record_iterator(record):
+#            print(tf.train.Example.FromString(example))
+#            sys.exit()
+
+
+            
             features = parse_prediction_result(example)
             result = decode_prediction_result(features)
             fid = result.key.eval().decode('utf-8')
