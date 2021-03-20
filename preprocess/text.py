717c6bf4e304ab17f754977d96f53ff4e46b96cf
rhoposit <j.williams@ed.ac.uk>
2021-03-20 13:08:35 +0000

    update pau

--- preprocess/text.py
+++ preprocess/text.py
@@ -30,7 +30,7 @@ _id_to_symbol = {i + 1: s for i, s in enumerate(symbols)}
 
 def text_to_sequence(text, cleaner):
     clean_text = cleaner(text)
-    print(clean_text)
+    print("clean_text", clean_text)
     return _symbols_to_sequence(clean_text), clean_text
 
 
