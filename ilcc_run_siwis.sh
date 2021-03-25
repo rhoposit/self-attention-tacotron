de61edbdf9b73fdf9173a36d6f066929b8de4db5
rhoposit <j.williams@ed.ac.uk>
2021-03-25 11:17:12 +0000

    adds siwis script cleaned up

--- ilcc_run_siwis.sh
+++ ilcc_run_siwis.sh
@@ -1,36 +1,48 @@
 
 
-
 # rsync special files to scratch space
 # generate the data on scratch space
 # rsync the data to home
 
-#rsync -ruva /home/s1738075/special/L1_dat_files/sys5_lang/siwis_552024/all_siwis_161_selected/ /disk/scratch/s1738075/data/
-rsync -ruva /home/s1738075/special/L1_dat_files/sys5_lang/siwis_552024/siwis_161_small /disk/scratch/s1738075/data/
-mkdir /disk/scratch/s1738075/data/siwis_source_small
-mkdir /disk/scratch/s1738075/data/siwis_target_small
-python preprocess_vqcodes.py --source-only --hparams=phoneme=flite,flite_binary_path='/home/s1738075/taco_modified/flite' /disk/scratch/s1738075/data/siwis_161_small /disk/scratch/s1738075/data/siwis_source_small siwis 0 161
+DATASET=vqcodes
+NAME=all_siwis_161
+ORIG_DATA=/home/s1738075/special/L1_dat_files/sys5_lang/siwis_552024/${NAME} 
 
-python preprocess_vqcodes.py --target-only --hparams=phoneme=flite,flite_binary_path='/home/s1738075/taco_modified/flite' /disk/scratch/s1738075/data/siwis_161_small /disk/scratch/s1738075/data/siwis_target_small siwis 0 161
-rsync -ruva /disk/scratch/s1738075/data/ /home/s1738075/data/
+SCRATCH=/disk/scratch/s1738075
+SCRATCH_DATA=/disk/scratch/s1738075/data
+SCRATCH_CHECK=/disk/scratch/s1738075/checkpoints
+SCRATCH_DATA_RAW=/disk/scratch/s1738075/data/${NAME}
+SOURCE_DATA=/disk/scratch/s1738075/data/${NAME}_source
+TARGET_DATA=/disk/scratch/s1738075/data/${NAME}_target
+CHECKPOINTS=/disk/scratch/s1738075/checkpoints/${NAME}
+LIST=/home/s1738075/taco_modified/self_attention_tacotron/examples/codes
+HPARAM_FILE=/home/s1738075/taco_modified/self_attention_tacotron/examples/codes/self-attention-tacotron.json
+OUTPUT_DIR=/home/s1738075/taco_modified/prediction/${NAME}
+
+
+#mkdir $SCRATCH
+#mkdir $SCRATCH_DATA
+#mkdir $SCRATCH_CHECK
+#mkdir ${CHECKPOINTS}
+#mkdir $SOURCE_DATA
+#mkdir $TARGET_DATA
+#rsync -ruva $ORIG_DATA $SCRATCH_DATA
+
+python preprocess_vqcodes.py --target-only --hparams=phoneme=flite,flite_binary_path='/home/s1738075/taco_modified/flite' $SCRATCH_DATA_RAW $TARGET_DATA siwis 0 161
+rsync -ruva $TARGET_DATA /home/s1738075/data/
+
+python preprocess_vqcodes.py --source-only --hparams=phoneme=flite,flite_binary_path='/home/s1738075/taco_modified/flite' $SCRATCH_DATA_RAW $SOURCE_DATA siwis 0 161
+rsync -ruva $SOURCE_DATA /home/s1738075/data/
+
+#rsync -ruva $SCRATCH_DATA_RAW /home/s1738075/data/
 exit
 
 
 export PYTHONPATH=/home/s1738075/taco_modified:/home/s1738075/taco_modified/tacotron2:/home/s1738075/taco_modified/self_attention_tacotron:/home/s1738075/taco_modified/multi_speaker_tacotron:/home/s1738075/miniconda3/envs/taco/lib/python3.6/site-packages:/home/s1738075/miniconda3/envs/taco/lib/python3.6/site-packages/mkl:$PYTHONPATH
 export TF_FORCE_GPU_ALLOW_GROWTH=true
-
-# TRAINING command to run
-DATASET=vqcodes
-SOURCE_DATA=/disk/scratch/s1738075/data/siwis_source_small
-TARGET_DATA=/disk/scratch/s1738075/data/siwis_target_small
-CHECKPOINTS=/disk/scratch/s1738075/checkpoints/siwis_small
-VCTK_SELECTED_LIST=/home/s1738075/taco_modified/self_attention_tacotron/examples/codes_siwis
-HPARAM_FILE=/home/s1738075/taco_modified/self_attention_tacotron/examples/codes_siwis/self-attention-tacotron.json
-
 export CUDA_VISIBLE_DEVICES=0
-python train.py --source-data-root=$SOURCE_DATA --target-data-root=$TARGET_DATA --selected-list-dir=$VCTK_SELECTED_LIST --checkpoint-dir=$CHECKPOINTS --hparam-json-file=$HPARAM_FILE
 
-#OUTPUT_DIR=/home/s1738075/taco_modified/prediction/siwis_selected0
-#python predict_code.py  --source-data-root=$SOURCE_DATA --target-data-root=$TARGET_DATA --selected-list-dir=$VCTK_SELECTED_LIST --checkpoint-dir=$CHECKPOINTS --hparam-json-file=$HPARAM_FILE --output-dir=$OUTPUT_DIR
+python train.py --source-data-root=$SOURCE_DATA --target-data-root=$TARGET_DATA --selected-list-dir=$LIST --checkpoint-dir=$CHECKPOINTS --hparam-json-file=$HPARAM_FILE
 
-#python postprocess_vqcodes.py siwis_selected0
+#python predict_code.py  --source-data-root=$SOURCE_DATA --target-data-root=$TARGET_DATA --selected-list-dir=$LIST --checkpoint-dir=$CHECKPOINTS --hparam-json-file=$HPARAM_FILE --output-dir=$OUTPUT_DIR
+#python postprocess_vqcodes.py siwis
