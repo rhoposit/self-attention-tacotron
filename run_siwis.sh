bc1ce6a515925ff6f3c0b6b891fbe5fb112726c5
rhoposit <j.williams@ed.ac.uk>
2021-04-03 16:47:44 +0100

    update

--- run_siwis.sh
+++ run_siwis.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #SBATCH --partition=ILCC_GPU                 # ILCC_GPU, CDT_GPU, ILCC_CPU, etc
 
-#SBATCH --job-name=taco_siwis                    # Job name
+#SBATCH --job-name=s_phones                    # Job name
 #SBATCH --mail-type=END,FAIL                 # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=j.williams@ed.ac.uk      # Where to send mail
 #SBATCH --ntasks=1                           # Run on a single machine
 #SBATCH --gres=gpu:1                         # request N gpus
 #SBATCH --cpus-per-task=1                    # require N cpus
 #SBATCH --mem=14000                          # Job memory request
-#SBATCH --time=05:00:00                      # Time limit hrs:min:sec
-#SBATCH --output=/home/s1738075/taco_modified/logs/siwis_161.out
-#SBATCH --error=/home/s1738075/taco_modified/logs/siwis_161.err
+#SBATCH --time=02:00:00                      # Time limit hrs:min:sec
+#SBATCH --output=/home/s1738075/taco_modified/logs/siwis_1024_phone.out
+#SBATCH --error=/home/s1738075/taco_modified/logs/siwis_1024_phone.err
 
 #echo "hello"
 
@@ -21,8 +21,9 @@ hostname
 conda activate taco
 
 DATASET=vqcodes
-NAME=all_siwis_161
+NAME=all_siwis_161_SP_phones_1024
 ORIG_DATA=/home/s1738075/special/L1_dat_files/sys5_lang/siwis_552024/${NAME}
+N=1025
 
 SCRATCH=/disk/scratch/s1738075
 SCRATCH_DATA=/disk/scratch/s1738075/data
